792e8f437cf49d9268efeb7451664a857f842bd4
AniAvazneli <avazneli.ani@gmail.com>
2022-08-11 22:38:59 +0400

    changed time

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 'use strict'
 
 // Set the date we're counting down to
-const countDownDate = new Date("Dec 15, 2022 00:00:00").getTime();
+const countDownDate = new Date("Mar 15, 2023 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
